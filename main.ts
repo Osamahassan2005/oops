e05e8535c120d90ba57d7e8f86e3d91b89ca44c5
osama hassan <hssan10997@gmail.com>
2024-06-08 16:11:20 +0500

    assigment 1 to 5

--- main.ts
+++ main.ts
@@ -1,18 +1,26 @@
 //....AS # 1
 //Install Node.js, TypeScript and VS Code on your computer.
+
 //..... AS # 2
+
 let personName : string = "osama"
 console.log(`Hello ${personName}, would you like to learn some typescipt today?`)
+
 //......AS # 3
+
 console.log("lowercase:"+ personName.toLowerCase())
 console.log("uppercase:"+ personName.toUpperCase())
 let titleCase = personName[0].toUpperCase() + personName.slice(1).toLowerCase();
 console.log("titlecase:"+ titleCase)
+
 //......AS # 4
+
 let famousPerson: string = `Dr Abdul Qadeer khan once said,"Pakistan's nuclear program is a symbol of our national 
 resolve and a guarantor of our national security."`
 console.log(famousPerson)
+
 //.....AS # 5 
+
 let famousPersonName : string = "Dr Abdul Qadeer khan"
 let message : string = `${famousPersonName} once said ,"Pakistan's nuclear program is a symbol of our national
  resolve and a guarantor of our national security."`
